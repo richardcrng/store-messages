df9036303504179b9fa39c8c9c3990c8b754cbba
Richard Ng <richardcrng@gmail.com>
2019-07-11 09:44:18 +0100

    map over messages in MessageThread

--- src/components/templates/MessageThread/MessageThread.js
+++ src/components/templates/MessageThread/MessageThread.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { F7Messages, F7Message, F7Page } from 'framework7-react';
+import { F7Messages, F7Message, F7Page, F7MessagesTitle } from 'framework7-react';
 
-function MessageThread() {
+function MessageThread({ messages = {} }) {
   return (
     <F7Page>
       <F7Messages>
-        <F7Message
-          type='sent'
-          name='Me'
-        >
-          <span>Message!!</span>
-        </F7Message>
+        {Object.entries(messages).map(([key, message]) => (
+          <React.Fragment key={key}>
+            <F7MessagesTitle><b>{key}</b></F7MessagesTitle>
+            <F7Message
+              type={message.sender === 'user' ? 'sent' : 'received'}
+            >
+              <span>{message.text}</span>
+            </F7Message>
+          </React.Fragment>
+        ))}
       </F7Messages>
     </F7Page>
   )
