5df6b15ad7ae6e6e50f1284304e90c75a1a3425f
Richard Ng <richardcrng@gmail.com>
2019-07-11 10:09:27 +0100

    change Message colour to green

--- src/components/molecules/Message/Message.js
+++ src/components/molecules/Message/Message.js
@@ -5,6 +5,7 @@ import MessageText from '../../atoms/MessageText';
 function Message({ children, sender }) {
   return (
     <F7Message
+      color='green'   // so that highlighting is visible
       tail
       type={sender === 'user' ? 'sent' : 'received'}
     >
