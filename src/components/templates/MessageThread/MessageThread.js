ecff56aef59f71963f177f22fcc276a0402df528
Richard Ng <richardcrng@gmail.com>
2019-07-11 09:53:00 +0100

    create Message molecule

--- src/components/templates/MessageThread/MessageThread.js
+++ src/components/templates/MessageThread/MessageThread.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { F7Messages, F7Message, F7Page, F7MessagesTitle } from 'framework7-react';
-import MessageText from '../../atoms/MessageText';
+import { F7Messages, F7Page } from 'framework7-react';
+import Message from '../../molecules/Message';
 
 function MessageThread({ messages = {} }) {
   return (
     <F7Page>
       <F7Messages>
         {Object.entries(messages).map(([key, { text, sender, createdAt }]) => (
-          <React.Fragment key={key}>
-            <F7MessagesTitle><b>{createdAt}</b></F7MessagesTitle>
-            <F7Message
-              tail
-              type={sender === 'user' ? 'sent' : 'received'}
-            >
-              <MessageText>{text}</MessageText>
-            </F7Message>
-          </React.Fragment>
+          <Message key={key} {...{ sender, text }} />
         ))}
       </F7Messages>
     </F7Page>
