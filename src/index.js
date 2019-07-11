7edf5b31f3fd9eae366c99255bffc829eb0224e3
Richard Ng <richardcrng@gmail.com>
2019-07-11 09:23:41 +0100

    import F7 CSS

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import Framework7 from 'framework7';
 // Import Framework7 React
 import Framework7React from 'framework7-react';
 
+// Import F7 CSS
+import 'framework7/css/framework7.bundle.min.css'
+
 // Init plugin
 Framework7.use(Framework7React)
 
