ee97dbdeda8cdfa374efa1bb15bd7ad125e3f23c
Richard Ng <richardcrng@gmail.com>
2019-07-11 13:31:48 +0100

    empty selection in onSelect callback

--- src/components/molecules/MessageText/MessageText.js
+++ src/components/molecules/MessageText/MessageText.js
@@ -4,31 +4,28 @@ import MessageCharacter from '../../atoms/MessageCharacter';
 import useSelectionRange from '../../../hooks/useSelectionRange';
 import { useMessageId } from '../../organisms/Message';
 import { useMessageThreadContext } from '../../templates/MessageThread';
-import usePrevious from '../../../hooks/usePrevious';
+import useSelection from '../../../hooks/useSelection';
 
-function MessageText({ children = '' }) {
+function MessageText({ children }) {
   const messageId = useMessageId()
   const ref = React.useRef()
   const { onSelect } = useMessageThreadContext()
 
   const range = useSelectionRange()
-  const rangeContents = range ? range.cloneContents() : { children: [] }
-  const prevContents = usePrevious(rangeContents)
+  const selection = useSelection()
 
   React.useEffect(() => {
     if (range) {
       // Check to see if length of contents has actually changed
-      if (rangeContents.children.length !== prevContents.children.length) {
-        const { commonAncestorContainer: container } = range;
-        if (container === ref.current) {
-          const timeoutHandler = onSelect({ container, messageId, range })
-          return function cleanup() {
-            clearTimeout(timeoutHandler)
-          }
+      const { commonAncestorContainer: container } = range;
+      if (container === ref.current) {
+        const timeoutHandler = onSelect({ container, messageId, range, selection })
+        return function cleanup() {
+          clearTimeout(timeoutHandler)
         }
       }
     }
-  }, [onSelect, range, ref ])
+  }, [ messageId, onSelect, range, ref, selection ])
 
   return (
     <span className={classes.MessageText} ref={ref}>
