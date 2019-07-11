cd048bc477eafd5f019e7d0ccf13b642e20316e4
Richard Ng <richardcrng@gmail.com>
2019-07-11 13:06:06 +0100

    prevent endless updates by checking to see if range contents have changed

--- src/hooks/useSelectionRange/useSelectionRange.js
+++ src/hooks/useSelectionRange/useSelectionRange.js
@@ -1,14 +1,42 @@
 import * as R from 'ramda'
+import React from 'react';
 import useSelection from '../useSelection/useSelection';
 
 function useSelectionRange({ selectionProvided } = {}) {
   const selectionHook = useSelection()
-
   const selection = R.defaultTo(selectionHook, selectionProvided)
 
+  const [range, setRange] = React.useState(getRangeFrom(selection))
+  
+  // Used to force refresh components hooked in
+  //  when selectionchange event fires
+  //  (since the Selection object itself does not update)
+  const [num, setNum] = React.useState(0)
+
+  const updateRange = React.useCallback(() => {
+    const newRange = getRangeFrom(selection)
+    setRange(newRange)    // typically it's the same object
+    setNum(n => n + 1)           // forces the re-render
+  }, [selection, setRange, setNum])
+
+  React.useEffect(() => {
+    document.addEventListener('selectstart', updateRange)
+    document.addEventListener('selectionchange', updateRange)
+
+    return function cleanup() {
+      document.removeEventListener('selectstart', updateRange)
+      document.removeEventListener('selectionchange', updateRange)
+    }
+  }, [updateRange])
+
+  return range
+}
+
+const getRangeFrom = selection => {
   return selection && selection.rangeCount
     ? selection.getRangeAt(0)
     : null
 }
+
 
 export default useSelectionRange
