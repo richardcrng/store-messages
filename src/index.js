9079a3911e36eda37c42693cc8e491d374eed715
Richard Ng <richardcrng@gmail.com>
2019-07-11 09:22:40 +0100

    basic framework7 setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Import Framework7 Core
+import Framework7 from 'framework7';
+
+// Import Framework7 React
+import Framework7React from 'framework7-react';
+
+// Init plugin
+Framework7.use(Framework7React)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
